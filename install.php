f78b7c550a27d30b84e92b09d3b27a34c83b39eb
Diogenesthecynic <diogenesthecynic@hotmail.com>
2013-11-21 21:09:56 -0500

    Initial database (install.php, settings.php)

---
+++ install.php
@@ -0,0 +1,91 @@
+<?php
+  /* Complete installation script
+   * 
+   * Starts the system off with a new, blank set of database tables
+   * 1. Create the database if it doesn't yet exist
+   * 2. Create the `users` table
+   * 3. Create the `books` table
+   * 4. Create the `history` table
+  */
+  require_once('settings.php');
+  
+  // Get a connection to the server (no specific database yet)
+  $dbConn = getPDO($dbHost, '', $dbUser, $dbPass);
+  
+  // 1. Create the database if it doesn't yet exist
+  $dbConn->prepare('
+    CREATE DATABASE IF NOT EXISTS ' . $dbName
+  )->execute();
+  // From now on, everything will be in that database
+  $dbConn->exec('USE ' . $dbName);
+  
+  // 2. Create the `users` table
+  // * These are identified by the user_id int
+  // (this should also be extended for Facebook integration)
+  $dbConn->exec('
+    CREATE TABLE IF NOT EXISTS `users` (
+      `user_id` INT(10) NOT NULL AUTO_INCREMENT,
+      `username` VARCHAR(127) NOT NULL,
+      `password` VARCHAR(127) NOT NULL,
+      `email` VARCHAR(127) NOT NULL,
+      `salt` VARCHAR(127) NOT NULL,
+      `role` INT(1),
+      PRIMARY KEY (`user_id`)
+    )
+  ');
+  
+  // 3. Create the `books` table
+  // This refers to the known information on a book
+  // * These are identified by their 13-digit ISBN number
+  $dbConn->exec('
+    CREATE TABLE IF NOT EXISTS `books` (
+      `isbn` INT(13) NOT NULL,
+      `name` VARCHAR(127),
+      `genre` VARCHAR(127),
+      `edition` VARCHAR(127),
+      PRIMARY KEY (`isbn`)
+    )
+  ');
+  
+  // 4. Create the `entries` table
+  // This contains the entries of books by users
+  // * These are also identified by their 13-digit ISBN number
+  // * Prices are decimels, as dollars
+  // * Condition may be one of the $bookConditions
+  // * Action may be one of the $bookActions
+  // (this should use `isbn` and `user_id` as foreign keys)
+  $dbConn->exec('
+    CREATE TABLE IF NOT EXISTS `entries` (
+      `isbn` INT(13) NOT NULL,
+      `user_id` INT(10) NOT NULL,
+      `name` VARCHAR(127) NOT NULL,
+      `price` DECIMAL(19,4),
+      `condition` ENUM(' . makeSQLEnum($bookConditions) . '),
+      `action` ENUM(' . makeSQLEnum($bookActions) . '),
+      PRIMARY KEY (`isbn`)
+    )
+  ');
+  
+  // 4. Create the `history` table
+  // This records an ordered history of all events, with associated rating
+  // Each event is listed twice: once for each user's perspective (for rating and action)
+  // * These are identified by their event_id (timestamps may not be unique)
+  // * Each event has the two users' ids, their event ratings, and the book isbn
+  // (this should use `isbn` and `user_id` as foreign keys)
+  $dbConn->exec('
+    CREATE TABLE IF NOT EXISTS `history` (
+      `event_id` INT(10) NOT NULL AUTO_INCREMENT,
+      `timestamp` TIMESTAMP NOT NULL,
+      `user_rater` INT(10) NOT NULL,
+      `user_rated` INT(10) NOT NULL,
+      `user_rating` ENUM(' . makeSQLEnum($historyRatings) . '),
+      `isbn` INT(13) NOT NULL,
+      `action` ENUM(' . makeSQLEnum($bookActions) . '),
+      PRIMARY KEY (`event_id`)
+    )
+  ');
+  
+  print_r(getUserInfo($dbConn, "a123$4+"));
+  print_r(getUserEntries($dbConn, "a1234"));
+  print_r(getUserRating($dbConn, "a1234"));
+?>
---
+++ settings.php
@@ -0,0 +1,104 @@
+<?php
+  /* General system settings
+   * Stores the database settings, such as host and table names
+  */
+  
+  // Database info
+  $dbHost = "localhost";
+  $dbUser = "root";
+  $dbPass = "";
+  $dbName = "WebSysProject";
+  
+  // Book particulars
+  $bookConditions = array('Terrible', 'Poor', 'Fair', 'Good', 'Very Good', 'Like New');
+  $bookActions = array('Buy', 'Sell', 'Trade');
+  $historyRatings = array('0', '1', '2', '3', '4', '5');
+  
+  
+  /* Quick PDO Functions
+  */
+  
+  // getPDO("dbHost", "dbName", "dbUser", "dbPass")
+  // Gets a new PDO object with the given settings
+  function getPDO($dbHost, $dbName, $dbUser, $dbPass) {
+    try {
+      $conn = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName, $dbUser, $dbPass);
+      // This helps with debugging (enables the PDOExceptions)
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } 
+    catch(PDOException $err) {
+      echo 'Error creating PDO: ' . $err->getMessage();
+    }
+    return($conn);
+  }
+  
+  
+  /* Quick SQL Functions
+  */
+  
+  // makeSQLEnum([...])
+  // Turns [7,14] into "'7', '14'"
+  function makeSQLEnum($myarr) {
+    if(is_string($myarr)) return $myarr;
+    return '\'' . implode($myarr, '\', \'') . '\'';
+  }
+  
+  // makeSQLSelects([...])
+  // Turns [7,14] into "`7`, `14`"
+  function makeSQLSelects($myarr) {
+    if(is_string($myarr)) return $myarr;
+    return '`' . implode($myarr, '`, `') . '`';
+  }
+  
+  // filterUserID(userID)
+  // Makes the id a string of only numbers
+  function filterUserID($userID) {
+    return str_replace(['+', '-'], '', filter_var('' . $userID, FILTER_SANITIZE_NUMBER_FLOAT));
+  }
+  
+  
+  /* Common SQL Queries
+  */
+  
+  // getUserInfo(PDO, #userID)
+  // Gets all the info about a user from the database  
+  function getUserInfo($dbConn, $userID) {
+    // Grab and return that userID's information from the database
+    return $dbConn->query('
+      SELECT * FROM `users`
+      WHERE `user_id` LIKE ' . filterUserID($userID) . '
+      LIMIT 1
+    ')->fetch();
+  }
+  
+  // getUserEntries(PDO, $userID[, 'action'])
+  // Gets all entries from a user
+  // Optionally filters on action type
+  function getUserEntries($dbConn, $userID, $action='%') {
+    // Prepare the query SQL
+    $query = '
+      SELECT * FROM `entries`
+      WHERE `user_id` LIKE ' . filterUserID($userID) . '
+      AND `action` LIKE :action
+    ';
+    // Create, run, and return a statement from the query
+    $stmnt = $dbConn->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $stmnt->execute(array(':action' => $action));
+    return $stmnt->fetchAll();
+  }
+  
+  // getUserRatings(PDO, $userID[, 'actions'])
+  // Returns the average of all ratings regarding a user
+  // Optionally filters on action type
+  function getUserRating($dbConn, $userID, $action='%') {
+    $query = '
+      SELECT AVG(`user_rating`) FROM `history`
+      WHERE `user_rated` LIKE ' . filterUserID($userID) . '
+      AND `action` LIKE :action
+    ';
+    // Create, run, and return a statement from the query
+    $stmnt = $dbConn->prepare($query, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+    $stmnt->execute(array(':action' => $action));
+    return $stmnt->fetchAll();
+  }
+?>
