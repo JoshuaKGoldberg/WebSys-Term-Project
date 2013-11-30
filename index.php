66eeb9f1a4e6185aded8ee4bb98354a05a7b4057
Diogenesthecynic <diogenesthecynic@hotmail.com>
2013-11-30 02:13:32 -0500

    Index wireframe

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Book Exchange</title>
+    <link rel="stylesheet" type="text/css" href="default.css">
+    <link href='http://fonts.googleapis.com/css?family=Doppio+One' rel='stylesheet' type='text/css'>
+    </head>
+  <body>
+    <!-- Header -->
+    <header>
+      <h1>book exchange or something idk</h1>
+    </header>
+    
+    <!-- Section 1 (main slogan) -->
+    <section id="center">
+      <div class="aligner">
+        <div class="container">
+          <div class="text">
+            <h1>Find textbooks to sell and trade, on your campus.</h1>
+          </div>
+        </div>
+      
+        <!-- Sign up / Log in -->
+        <div id="button">
+          <div id="button_inside">
+            <div class="half" onclick="scrollToSection('signup')">join!</div>
+            <div class="half">log in</div>
+              <!--
+                <input type='text' value='username' name='username' />
+                <input type='text' value='password' name='password' />
+                <input type='submit' value='log in' />
+              -->
+          </div>
+        </div>
+      </div>
+    
+    </section>
+    
+    <!-- Section 2 (sign up) -->
+    <section id="signup">
+      <div class="aligner">
+        <div class="container">
+          <div class="text">
+            <p>Sign-up stuff goes here. Small paragraph on the right, forms on the left? maybe</p>
+          </div>
+        </div>
+    </section>
+    
+    <!-- Section 2 (more) -->
+    <section id="more">
+      <div class="aligner">
+        <div class="container">
+          <div class="text">
+            <p>More information on what the site does, explanation of the term project, etc? maybe</p>
+          </div>
+        </div>
+    </section>
+    
+    <!-- Footer -->
+    <footer>
+      (WebSys term project)
+      <hr />
+      haters gonna hate
+    </footer>
+  </body>
+</html>
