52605128a9e49d716dee6fc2d16c5706757eddf0
Diogenesthecynic <diogenesthecynic@hotmail.com>
2013-12-03 14:39:55 -0500

    Footer / JS formatting fix

--- php/html_footer.inc.php
+++ php/html_footer.inc.php
@@ -14,7 +14,11 @@ function html_print_footer($js=false) {
   echo '  </body>' . PHP_EOL;
   echo PHP_EOL;
   
-  // Add the necessary JS files
+  // Always include search, requests, and JQuery
+  echo '  <script type="text/javascript" src="js/search.js"></script>' . PHP_EOL;
+  echo '  <script type="text/javascript" src="js/requests.js"></script>' . PHP_EOL;
+  echo '  <script type="text/javascript" src="js/jquery-2.0.3.min.js"></script>' . PHP_EOL;
+  // Add any other necessary JS files
   foreach($js as $filename)
     echo '  <script type="text/javascript" src="js/' . $filename . '.js"></script>' . PHP_EOL;
   
