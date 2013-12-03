565455ab712ed112b88d498cd4b95ac1fba5898d
Diogenesthecynic <diogenesthecynic@hotmail.com>
2013-12-03 16:26:31 -0500

    Small change to template.php

--- template.php
+++ template.php
@@ -1,8 +1,12 @@
 <?php 
   require_once('php/html_helpers.php');
-  page_start(array("mainpage"));
+  page_start(array());
 ?>
+  
+  <div class="main_standard width_standard">
     
     <h1>Content goes here!</h1>
+    
+  </div>
     
 <?php page_end(); ?>
