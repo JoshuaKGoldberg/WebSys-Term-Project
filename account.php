032a70f8e1c4a7d1e274e8dc907aea61286e100e
FTLspawn <macgregor.evan@gmail.com>
2013-12-05 15:12:11 -0500

    Added form to change user information, but...
    The form doesn't actually do anything. So if anyone wants to add that
    functionality, be my guest. Also made some CSS changes.

--- account.php
+++ account.php
@@ -2,20 +2,56 @@
   require_once('php/html_helpers.php');
   page_start(array("account"));
   ensure_logged_in();
+  $dbConn = getPDOQuick();
+  setlocale(LC_MONETARY, 'en_US');
+
+  function accountPrintRow($left, $right, $classleft='half', $classright='half') {
+    echo '<div class="form_row ' . $classleft . ' ' . $classright . '">' . PHP_EOL;
+    echo accountPrintInput($left, $classleft);
+    echo accountPrintInput($right, $classright);
+    echo '</div>' . PHP_EOL;
+  }
+
+  function accountPrintInput($name, $classname='half') {
+    $name = preg_replace("/[^A-Za-z_]/", '', $name);
+    $clean_name = str_replace("_", " ", $name);
+    echo '<div class="input_holder input_' . $classname . '">' . PHP_EOL;
+    if ($name == "password" || $name == "confirm_password") {
+      echo '  <input name="' . $name . '" id="input_' . $name . '" type="password" class="form_input" placeholder="' . $clean_name . '" />' . PHP_EOL;
+    } else {
+      echo '  <input name="' . $name . '" id="input_' . $name . '" type="text" class="form_input" placeholder="' . $clean_name . '" />' . PHP_EOL;
+    }
+    echo '  <aside class="form_aside">' . $clean_name . '</aside>' . PHP_EOL;
+    echo '</div>' . PHP_EOL;
+  }
+
+  if (isset($_POST) && isset($_POST['change']) && $_POST['change'] == "Submit") {
+    
+  }
 
   $username = $_SESSION['username'];
   $user_id = $_SESSION['user_id'];
   if(isset($_SESSION['email'])) $email = $_SESSION['email'];
   if(isset($_SESSION['major'])) $major = $_SESSION['major'];
-  if(isset($_SESSION['fname']) && isset($_SESSION['lname'])) $name = $_SESSION['fname'] . " " . $_SESSION['lname'];
+  if(isset($_SESSION['fname']) && isset($_SESSION['lname'])) $fullname = $_SESSION['fname'] . " " . $_SESSION['lname'];
 
-  $dbConn = getPDOQuick();
-  setlocale(LC_MONETARY, 'en_US');
 ?>
-
+  <p id="before_lists"><a id='edit' href=''>Edit your account...</a><p>
+  <div id='account-info' class="width_standard main_standard body_standard" class="main_standard width_standard" style="display:none">
+    <form action='account.php' method='post'>
+      <?php
+        //accountPrintRow('first_name', 'last_name');
+        accountPrintRow('email', 'username');
+        //accountPrintRow('major', 'year');
+        accountPrintRow('password', 'confirm_password');
+      ?>
+      <input type="submit" name="change" value="Submit" />
+    </form>
+  </div>
   <div id="listwrapper">
     <div id="wishlist" class="booklist">
       <h3>Books you want...</h3>
+
       <?php
 
       $user_wishes = dbEntriesGet($dbConn, $user_id, "Wish");
@@ -51,9 +87,11 @@
       }
 
       ?>
+
     </div>
     <div id="tradelist" class="booklist">
       <h3>Books you're trading...</h3>
+
       <?php
 
       $user_trades = dbEntriesGet($dbConn, $user_id, "Trade");
@@ -91,8 +129,19 @@
       }
 
       ?>
+
     </div>
     <p id="after_lists">You can also <a href="import.php">import</a> books to our database.</p>
   </div>
+  <script>
+    window.onload = function() {
+      var a = document.getElementById("edit");
+      a.onclick = function() {
+        $("#account-info").slideToggle();
+        $("#edit").slideUp();
+        return false;
+      }
+    }
+  </script>
 
 <?php page_end(); ?>
